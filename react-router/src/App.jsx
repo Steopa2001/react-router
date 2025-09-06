027ba404816d8594c169f5abe65752256467b427
Steopa2001 <moraru495@gmail.com>
2025-09-06 13:12:04 +0200

    feat: import link, NavLink in Navbar.jsx

--- react-router/src/App.jsx
+++ react-router/src/App.jsx
@@ -1,7 +1,15 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/products" element={<Products />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ react-router/src/components/Navbar.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import {link, NavLink} from 'react-router-dom'
+
+const Navbar = () => {
+
+}
+
+export default Navbar

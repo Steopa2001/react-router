d019bbcb7082d4dc54852be1a84aeae39f53963b
Steopa2001 <moraru495@gmail.com>
2025-09-06 13:17:38 +0200

    feat: import Navbar.jsx in the various components

--- react-router/src/components/Navbar.jsx
+++ react-router/src/components/Navbar.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
-import {link, NavLink} from 'react-router-dom'
+import React from "react";
+import { link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
+  return (
+    <nav>
+      <ul className="list-unstyled d-flex">
+        <li>
+          <NavLink to={"/"}>Home</NavLink>
+        </li>
+        <li>
+          <NavLink to={"/about"}>About</NavLink>
+        </li>
+        <li>
+          <NavLink to={"/products"}>Products</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
-}
-
-export default Navbar
+export default Navbar;
---
+++ react-router/src/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <>
+    <Navbar/>
+    <h1>About</h1>
+    </>
+  )
+}
+
+export default About;
